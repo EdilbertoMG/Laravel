4b2fd01c0ab8806391d477dd0b5b8b7ec194250b
edilbertomgutierrez <edilbertomarrugo03@hotmail.com>
2018-10-13 03:57:09 -0500

    Creando Rutas y Vistas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('nosotros', function () {
+    return view('about');
+})->name('about');
+
+Route::get('servicios', function () {
+    return view('service');
+})->name('service');
+
+Route::get('contactos', function () {
+    return view('contact');
+})->name('contact');
